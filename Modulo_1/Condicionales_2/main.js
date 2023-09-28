8973e469afb3f2a92be669f2e3ea243637b1727e
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2023-09-27 22:53:37 -0300

    adding exercises solutions

--- Modulo_1/Condicionales_2/main.js
+++ Modulo_1/Condicionales_2/main.js
@@ -136,6 +136,54 @@ PRACTICA
 => ADIVINA EL NUMERO
 - Crear un juego en el que se le pide al usuario adivinar un numero random del 0 al 10
 - Si el usuario gana, emitir un mensaje que diga felicitaciones adivinaste. Caso contrario, lo siento perdiste.
+
 - Ahora le subimos la dificultad. Vamos a darle al usuario tres vidas como chances de adivinar. Con cada fracaso debe restarse una vida, en caso de ganar mantener el mensaje de felicidades. Si se queda sin vidas, perdio.
 
 */
+
+// recursividad
+
+// const randomNumber = Math.round(Math.random() * 10)
+// let lives = 3
+
+// const guessTheRandomNumber = () => {
+//     const chosenNumber = parseInt(prompt("Adivina el numero entre 0 y 10"))
+
+//     if (randomNumber === chosenNumber) {
+//         alert("Felicitaciones ganaste!!")
+//     } else {
+//         lives--
+//         if (lives > 0) {
+//             guessTheRandomNumber()
+//         } else {
+//             alert("Lo siento, perdiste")
+//         }
+//     }
+// }
+
+// guessTheRandomNumber()
+
+/*
+Necesitamos un programa que pida ingresar un número de partida y una cantidad, mediante los siguientes mensajes:
+
+Ingresá un número
+Ingresá una cantidad
+Con esta información, el programa deberá incrementar el número cinco veces la cantidad y mostrar cinco mensajes seguidos que muestren el número del mensaje y el total incrementado hasta el momento. Por ejemplo si ingresamos el 6 como número y el 1 como cantidad, debería mostrar lo siguiente:
+
+'Incremento 1: 7'
+'Incremento 2: 8'
+'Incremento 3: 9'
+'Incremento 4: 10'
+'Incremento 5: 11'
+*/
+
+const numero = parseInt(prompt("Ingrese un numero")) // 6
+const incremento = parseInt(prompt("Ingrese un incremento")) // 2
+
+const incrementar = () => {
+    alert(`Incremento 1: ${numero + incremento}`)
+    alert(`Incremento 2: ${numero + (incremento*2)}`)
+    alert(`Incremento 3: ${numero + (incremento*3)}`)
+    alert(`Incremento 4: ${numero + (incremento*4)}`)
+    alert(`Incremento 5: ${numero + (incremento*5)}`)
+}
