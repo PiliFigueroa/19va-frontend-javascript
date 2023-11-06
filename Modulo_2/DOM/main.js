e93f41468ef76c465750e8915e99b5702e8653cf
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2023-11-06 20:15:46 -0300

    adding exercise resolution

--- Modulo_2/DOM/main.js
+++ Modulo_2/DOM/main.js
@@ -31,3 +31,81 @@ const $$ = (selector) => document.querySelectorAll(selector)
 //         e.target.parentElement.remove()
 //     })
 // }
+
+/*
+### 📈 Total de gastos
+
+Crear un programa que muestre:
+
+- un input de números
+- un botón que diga `Agregar gasto/ganancia`
+- una lista `ul`
+- un elemento `p`
+
+Cuando se hace click en el botón, se debe agregar el valor del input a un array y actualizar la lista y el elemento `p`.
+
+Para actualizar la lista se debe:
+
+- borrar todo lo que contenga
+- recorrer el array y por cada ítem
+- insertar un ítem de lista `li` con el valor del ítem del array
+
+El elemento `p` se actualiza con el total de la suma de los valores del array.
+*/
+
+// inicializo array vacio
+const operations = []
+
+const addOperation = () => {
+    const valueInput = $("#input-number").valueAsNumber
+    operations.push(valueInput)
+}
+
+const totalBalance = (operations) => {
+    let acc = 0
+    for (const operation of operations) {
+        acc += operation
+    }
+    return acc
+}
+
+const generateList = (operations) => {
+    $("#list").innerHTML = ""
+    for (const operation of operations) {
+        $("#list").innerHTML += `<li>${operation}</li>`
+    }
+}
+
+$("#add-operation").addEventListener("click", (e) => {
+    e.preventDefault()
+    addOperation()
+    generateList(operations)
+    $("#total-cash").innerText = totalBalance(operations)
+})
+
+// $("#add-operation").addEventListener("click", (e) => {
+//     e.preventDefault()
+//     // guardo el valor numerico del input
+//     const valueInput = $("#input-number").valueAsNumber
+//     // pusheo el dato ingresado a mi array
+//     operations.push(valueInput)
+//     // reset del input
+//     $("#input-number").value = ""
+
+//     // vaciar el <ul> constantemente para que no se repitan los valores
+//     $("#list").innerHTML = ""
+
+//     // preparo un acumulador para hacer el calculo de las operaciones
+//     let acc = 0
+
+//     // itero por las operaciones
+//     for (const operation of operations) {
+//         // agrego el li a la lista con la operacion
+//         $("#list").innerHTML += `<li>${operation}</li>`
+//         // agrego el valor a mi acumulador
+//         acc += operation
+//     }
+
+//     // muestro el balance total
+//     $("#total-cash").innerText = `$${acc}`
+// })
