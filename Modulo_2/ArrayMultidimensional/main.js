464adec17d9c3fcf13977337ad61600e5da261b1
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2023-11-06 22:04:07 -0300

    adding multidimensional array

---
+++ Modulo_2/ArrayMultidimensional/main.js
@@ -0,0 +1,15 @@
+// ARRAY MULTIDIMENSIONAL
+
+// const greenGrocery = [
+//     ["papa", "zanahoria", "cebolla", "acelga"],
+//     ["manzana", "naranja", "banana", "pera"]
+// ]
+
+// // console.log(greenGrocery)
+// console.log(greenGrocery[0][1])
+
+// for (const array of greenGrocery) {
+//     for (const elemento of array) {
+//         console.log(elemento)
+//     }
+// }
